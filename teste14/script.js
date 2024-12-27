23c01d7b7aa0a875d73d6c59f36d38af12f3eb3a
Luiz Fernando Mendes Alberton <001006437@copobras.local>
2024-12-27 09:33:59 -0300

    teste de planilhas

---
+++ teste14/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // Inicializa o DataTable para a tabela de funcionários
+    $('#funcionarios').DataTable({
+        "language": {
+            "lengthMenu": "Exibir _MENU_ registros por página",
+            "zeroRecords": "Nenhum resultado encontrado",
+            "info": "Exibindo página _PAGE_ de _PAGES_",
+            "infoEmpty": "Nenhum registro disponível",
+            "infoFiltered": "(filtrado de _MAX_ registros no total)",
+            "search": "Pesquisar:",
+            "paginate": {
+                "previous": "Anterior",
+                "next": "Próximo"
+            }
+        }
+    });
+});
